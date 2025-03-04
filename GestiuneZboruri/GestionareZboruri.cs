9d33265e9385ad58e469b25c34fed501d2c4241b
suche <suche@DENYS>
2025-03-04 08:25:51 +0200

    Add project files.

---
+++ GestiuneZboruri/GestionareZboruri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestiuneZboruri
+{
+    class GestionareZboruri
+    {
+        public Zbor[] Zboruri;
+
+        // Constructor fara parametri
+        public GestionareZboruri(int capacitate)
+        {
+            Zboruri = new Zbor[capacitate];
+        }
+
+        // Constructor cu parametri
+        public GestionareZboruri(Zbor[] zboruri)
+        {
+            Zboruri = zboruri;
+        }
+    }
+}
---
+++ GestiuneZboruri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestiuneZboruri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var zbor1 = new Zbor(1023, "AirFrance", "Paris CDG", "Bucharest OTP", "13.03.2025 9:45", "13.03.2025 12:30");
+            string s = zbor1.Info();
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+            // Instantierea unui obiect de gestionare a zborurilor cu capacitatea de 5 zboruri
+           
+        }
+    }
+}
